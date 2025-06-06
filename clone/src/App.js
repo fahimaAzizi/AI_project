64d67b01568251f9279ef15cb71376a6bd5cd267
fahimaAzizi <118080917+fahimaAzizi@users.noreply.github.com>
2025-06-06 20:45:28 +0500

    Update App.js

--- clone/src/App.js
+++ clone/src/App.js
@@ -2,7 +2,30 @@ import './App.css';
 
 function App() {
   return (
-   <div className ="App"></div>
+   <div className ="App">
+    <div className="sidebar">
+      
+      <div className='upperSid'>
+        <div className='upperSideTop'><img src={gptLogo} className='logo'/>chatGPT</div>
+       <button className='midBtn'><img className='addBtn' src=''></img>New chat</button>
+       <div className='upperSideBttom'>
+       <button className='query'>What is Programming?</button>
+        <button className='query'>What is Programming?</button>
+       </div>
+
+      </div>
+      <div className='lowerSide'>
+        <div className='listItems'><img src='' className='listitemsImg'></img>Home</div>
+        <div className='listItems'><img src='' className='listitemsImg'></img>Saved</div>
+        <div className='listItems'><img src='' className='listitemsImg'></img>Upgrade to prok</div>
+        
+
+      </div>
+      <div className='main'>
+
+      </div>
+    </div>
+   </div>
   );
 }
 
